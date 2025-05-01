ea69e2dcc9bb0fcd0241faac68b54e6f8131a5a1
MJ1251 <11308571+MicDropBaam@users.noreply.github.com>
2025-04-30 21:37:04 -0700

    Add files via upload
    Physics

--- Play.h
+++ Play.h
@@ -1,7 +1,5 @@
 #ifndef PLAY_H
 #define PLAY_H
-
-//Third-party headers
 #include "WarpEngine.h"
 
 void PlayLoad(WEGameData* pGameData);
@@ -9,6 +7,7 @@ void PlayInit(WEGameData* pGameData);
 void PlayUpdate(WEGameData* pGameData, float dt);
 void PlayShutdown(WEGameData* pGameData);
 void PlayUnload(WEGameData* pGameData);
-void PlayDraw(void);
+
+void Draw(float dt);
 
 #endif
